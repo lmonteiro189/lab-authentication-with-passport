845cd9056430f7e665d7f84a6549976a3492b651
Lais Monteiro <lmonteiro189@gmail.com>
2020-05-14 14:21:03 +0100

    solved

--- middleware/route-guard.js
+++ middleware/route-guard.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// Route Guard Middleware
-// This piece of middleware is going to check if a user is authenticated
-// If not, it sends the request to the app error handler with a message
 module.exports = (req, res, next) => {
   if (req.user) {
     next();
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const { Router } = require('express');
-const router = Router();
+const router = new Router();
+const routeGuard = require('./../middleware/route-guard');
 
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Hello World!' });
+});
+
+router.get('/private', routeGuard, (req, res, next) => {
+  res.render('private');
 });
 
 module.exports = router;
